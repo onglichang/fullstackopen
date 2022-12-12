1a24c9004ff7aa9dfe384e52bc54a37e6500e83d
Li-Chang Ong <onglichang@Li-Changs-MacBook-Pro.local>
2022-12-12 17:23:02 +0800

    feat: add functionality to buttons

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,9 +9,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
