0d1876b6a2f3117d25d9f802c3ba85fbee9b37cf
Li-Chang Ong <onglichang@Li-Changs-MacBook-Pro.local>
2022-12-12 18:23:13 +0800

    feat: add a motivational quote section

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -48,12 +48,30 @@ const Statistics = ({good, neutral, bad}) => {
 }
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const generateAnecdote = () => {
+    const num = Math.floor(Math.random() * anecdotes.length)
+    setSelected(num)
+  }
+
   return (
+    <>
     <div>
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good"></Button>
@@ -61,6 +79,12 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text="bad"></Button>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
+    <div>
+      <h1>quote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={generateAnecdote} text="Generate a new quote" />
+    </div>
+    </>
   )
 }
 
