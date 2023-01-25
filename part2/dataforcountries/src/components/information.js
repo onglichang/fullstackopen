3cbd1c3318c8b91ffc8454e0ad7b39a62b50e7b3
Li-Chang Ong <onglichang@Li-Changs-MacBook-Pro.local>
2023-01-25 16:32:04 +0800

    refactor: Country component to use readable logic

--- part2/dataforcountries/src/components/information.js
+++ part2/dataforcountries/src/components/information.js
@@ -1,19 +1,18 @@
 import Country from './country'
 
 const Information = ({countries}) => {
-    return (
-        <>
-            {countries.length === 0 ?
-            null :
-            countries.length === 1 ? 
-            <Country country={countries[0]}></Country> :
-            countries.length > 10 ?
-            <div>Too many matches, specify another filter</div> :
-            countries.map((country) => (
-            <div key={country.name.official}>{country.name.common}{" "}</div>
-            ))}  
-        </> 
-    )
+    if (!countries) {
+        return null
+    }
+
+    if (countries.length === 1) {
+        return <Country country={countries[0]}></Country>
+    } else if (countries.length > 10) {
+        return <div>Too many matches, specify another filter</div>
+    } else {
+        return countries.map((country) => (
+        <div key={country.name.official}>{country.name.common}{" "}</div>))
+    }
 }
 
 export default Information
