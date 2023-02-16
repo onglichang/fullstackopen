492d826ecc57e1c0071af2ab3f393a74d5008a53
Li-Chang Ong <onglichang@Li-Changs-MacBook-Pro.local>
2023-02-16 15:40:43 +0800

    feat: add phonebook-be

---
+++ part3/phonebook-be/index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+  
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    }
+    response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+  
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+}
+
+const getRandomInt = (max) => {
+    const newId = Math.floor(Math.random() * max)
+    if (!persons.find(person => person.id === newId)) {
+        return newId
+    } 
+    return getRandomInt(max)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'field missing' 
+        })
+    } else if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    }
+
+    const person = {
+        "id": getRandomInt(1000),
+        "name": body.name, 
+        "number": body.number
+    }
+    
+    persons = persons.concat(person)
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const noOfPersons = persons.length
+    const time = new Date()
+    const personInfo = `Phonebook has info for ${noOfPersons} people \n${time}` 
+    
+    response.end(personInfo)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
