53f728723a4a513f351abb94661ce1933b2751f2
Li-Chang Ong <onglichang@li-changs-mbp.lan>
2023-02-27 18:15:14 +0800

    connect DB to BE and correct BE to FE connection

--- part3/phonebook-be/index.js
+++ part3/phonebook-be/index.js
@@ -19,36 +19,15 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 
 app.use(express.static('build'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
   
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+}) 
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -61,10 +40,13 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
+    // const id = Number(request.params.id)
+    // persons = persons.filter(note => note.id !== id)
   
-    response.status(204).end()
+    // response.status(204).end()
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
 const generateId = () => {
@@ -89,20 +71,21 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({ 
             error: 'field missing' 
         })
-    } else if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({ 
-            error: 'name must be unique' 
-        })
-    }
+    } 
+    // else if (persons.find(person => person.name === body.name)) {
+    //     return response.status(400).json({ 
+    //         error: 'name must be unique' 
+    //     })
+    // }
 
-    const person = {
-        "id": getRandomInt(1000),
+    const person = new Person({
         "name": body.name, 
         "number": body.number
-    }
+    })
     
-    persons = persons.concat(person)
-    response.json(persons)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 app.get('/info', (request, response) => {
